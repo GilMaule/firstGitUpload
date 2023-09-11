7103151b4214c38a30f7231b06c911525653efae
Gilvane Maule <gilvanejoin@gmail.com>
2023-09-11 20:50:28 -0300

    commit inicial

---
+++ for.js
@@ -0,0 +1,5 @@
+//for(inicio; teste; incremento){  }
+
+for(i=0; i < 10; i++){
+    console.log(`i=${i}`)
+}
---
+++ while_doWhile.js
@@ -0,0 +1,13 @@
+var i = 0
+
+while( i < 10){         // enquanto i for meno que 10 faça:{bloco}
+    i++
+}
+
+console.log('\n')
+
+var j = 0           
+do {                      //faça:{bloco} enquanto condiçao for verdadeira
+    console.log(`j=${j}`)
+    j++
+}while(j<10)
